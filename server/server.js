c5460845c5d6d40951a2ef7bfe4825f9cac6199e
Shobair Abidi <shobair.abidi@gmail.com>
2021-11-11 18:10:04 -0500

    updated Server.js for new connection

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-
+const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
@@ -16,11 +15,8 @@ const server = new ApolloServer({
 });
 
 server.applyMiddleware({ app });
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// Serve up static assets
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
 if (process.env.NODE_ENV === 'production') {
